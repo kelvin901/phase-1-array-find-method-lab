8d95b96decc6863741da284b2468b13307f68fbe
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-19 20:12:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(arr) {
+  const win = arr.find(obj => obj.result === "W");
+  return win ? win.year : undefined;
+}
+const games = [
+  { year: 2022, result: "L" },
+  { year: 2021, result: "W" },
+  { year: 2020, result: "L" },
+  { year: 2019, result: "W" },
+];
+
+console.log(superbowlWin(games)); // Output: 2021
+
